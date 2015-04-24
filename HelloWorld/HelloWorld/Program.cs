ca21fcd134b36a20899823a5b37e54b987b43989
Daphne Ng <daphnentw@hotmail.com>
2015-04-24 16:26:53 -0400

    Added read

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -12,6 +12,9 @@ namespace HelloWorld
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("The current time is {0}.", DateTime.Now);
+            Console.Write("Who is it? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}", name);
             Console.ReadLine();
         }
     }
